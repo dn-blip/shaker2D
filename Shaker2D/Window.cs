b9544048626c789b863e87b7dc9cf7af15d62771
dn-blip <nzekwe.da@wcs-g.com>
2025-01-06 12:30:08 -0700

    Update Window.cs

--- Shaker2D/Window.cs
+++ Shaker2D/Window.cs
@@ -1,1 +1,14 @@
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 
+namespace Shaker2D;
+
+public sealed class Window : GameWindow, IDisposable {
+  public int Width;
+  public int Height;
+  readonly string Title;
+
+ public Window(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) { }
+}
